89ba8723eae5e9acb460a63bf7be3f14be5959aa
Mattia Natale <mattianatale07@gmail.com>
2025-02-13 18:58:17 +0100

    feat: add milestone 3

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,21 +1,46 @@
 export default function Movie({ movie, getFlagImage }) {
+  const baseImageUrl = "https://image.tmdb.org/t/p/";
+  const imageSize = "w342";
+  const moviePosterUrl = movie.poster_path
+    ? `${baseImageUrl}${imageSize}${movie.poster_path}`
+    : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+
+  const starsArray = [0, 1, 2, 3, 4];
+
+  const vote = Math.min(Math.floor(movie.vote_average / 2 + 0.5), 5) || 1;
+
   return (
     <>
-      <h3>{movie.title}</h3>
-      <p>
-        <strong>Titolo originale:</strong> {movie.original_title}
-      </p>
       <div>
-        <strong>Lingua originale:</strong>
-        <img
-          className="img-flag"
-          src={getFlagImage(movie.original_language)}
-          alt={movie.original_language}
-        />
+        <h3>{movie.title}</h3>
+        <p>
+          <strong>Titolo originale:</strong> {movie.original_title}
+        </p>
+        <div>
+          <img src={moviePosterUrl} alt={movie.title} />
+        </div>
+        <div>
+          <strong>Lingua originale:</strong>
+          <img
+            className="img-flag"
+            src={getFlagImage(movie.original_language)}
+            alt={movie.original_language}
+          />
+        </div>
+        <div className="vote-container">
+          <strong>Voto:</strong>
+          <div className="stars-container">
+            {starsArray.map((index) => (
+              <i
+                key={index}
+                className={`vote-star ${
+                  index < vote ? "fas fa-star" : "far fa-star empty"
+                }`}
+              ></i>
+            ))}
+          </div>
+        </div>
       </div>
-      <p>
-        <strong>Voto:</strong> {movie.vote_average}
-      </p>
     </>
   );
 }
--- src/components/TvSeries.jsx
+++ src/components/TvSeries.jsx
@@ -1,10 +1,21 @@
 export default function TvSeries({ series, getFlagImage }) {
+  const baseImageUrl = "https://image.tmdb.org/t/p/";
+  const imageSize = "w342";
+  const seriesPosterUrl = series.poster_path
+    ? `${baseImageUrl}${imageSize}${series.poster_path}`
+    : "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg";
+
+  const starsArray = [0, 1, 2, 3, 4];
+
+  const vote = Math.min(Math.floor(series.vote_average / 2 + 0.5), 5) || 1;
+
   return (
     <>
       <h3>{series.name}</h3>
       <p>
         <strong>Titolo originale:</strong> {series.original_name}
       </p>
+      <img src={seriesPosterUrl} alt={series.name} />
       <div>
         <strong>Lingua originale:</strong>
         <img
@@ -13,9 +24,19 @@ export default function TvSeries({ series, getFlagImage }) {
           alt={series.original_language}
         />
       </div>
-      <p>
-        <strong>Voto:</strong> {series.vote_average}
-      </p>
+      <div className="vote-container">
+        <strong>Voto:</strong>
+        <div className="stars-container">
+          {starsArray.map((index) => (
+            <i
+              key={index}
+              className={`vote-star ${
+                index < vote ? "fas fa-star" : "far fa-star empty"
+              }`}
+            ></i>
+          ))}
+        </div>
+      </div>
     </>
   );
 }
